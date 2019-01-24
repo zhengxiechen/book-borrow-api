cbf8a6e7143b4cd58d78b564cf2fdaaffa98ceec
Agam Chopra <Acdcagam@gmail.com>
2019-01-23 20:43:26 -0500

    Added 1st route

--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,23 @@
-import * as express from 'express';
-import * as bodyParser from 'body-parser'; //used to parse the form data that you pass in the request
+import * as express from "express";
+import * as bodyParser from "body-parser";
+import { mainRoutes } from "./routes/MainRoutes";
 
 class App {
+  public app: express.Application;
 
-    public app: express.Application;
+  constructor() {
+    this.app = express();
+    this.config();
+  }
 
-    constructor() {
-        this.app = express(); //run the express instance and store in app
-        this.config();
-    }
-
-    private config(): void {
-        // support application/json type post data
-        this.app.use(bodyParser.json());
-        //support application/x-www-form-urlencoded post data
-        this.app.use(bodyParser.urlencoded({
-            extended: false
-        }));
-    }
-
+  private config(): void {
+    // support application/json
+    this.app.use(bodyParser.json());
+    //support application/x-www-form-urlencoded post data
+    this.app.use(bodyParser.urlencoded({ extended: false }));
+    // Routing
+    this.app.use("/", mainRoutes);
+  }
 }
 
 export default new App().app;
