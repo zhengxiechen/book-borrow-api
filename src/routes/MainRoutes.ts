73655b304ac1827f51e2edbc27400bc28b05f18b
Keith Fung <keith.robert.fung@gmail.com>
2019-01-24 19:52:40 -0500

    Update TSLint Rules to AirBnB
    Update TSLint rules and change the files to match

--- src/routes/MainRoutes.ts
+++ src/routes/MainRoutes.ts
@@ -1,5 +1,5 @@
-import * as express from "express";
-import { mainController } from "../controllers/MainController";
+import * as express from 'express';
+import { mainController } from '../controllers/MainController';
 
 class MainRoutes {
   public router: express.Router = express.Router();
@@ -9,8 +9,8 @@ class MainRoutes {
   }
 
   private config(): void {
-    this.router.get("/", (req: express.Request, res: express.Response) =>
-      mainController.root(req, res),
+    this.router.get('/', (req: express.Request, res: express.Response) =>
+      mainController.root(req, res)
     );
   }
 }
